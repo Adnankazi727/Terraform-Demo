b79459ebe8320b49c2fc2d9dbd382852d43e22d5
Adnankazi727 <adnankaz727@gmail.com>
2025-07-31 12:10:26 +0400

    update

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,11 @@
 
 provider "aws" {
-    region = "us-east-1"
+    region = "us-east-2"
 }
 
 resource "aws_instance" "my-instance" {
-    ami = "ami-020cba7c55df1f615"
+    ami = "ami-08ca1d1e465fbfe0c"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["sg-0e541e5e9aa9d774a"]
+    vpc_security_group_ids = ["sg-035405d565570ee56"]
 }
 
